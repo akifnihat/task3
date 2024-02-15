a50cff0619988d918121926acd32deb5dc73177d
akifnihat <akifst@code.edu.az>
2024-02-15 17:03:19 +0400

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("Hello, World!");
+#region 5
+//int a = 1234;
+//int count = 0;
+//while (a > 0)
+//{
+//    a /= 10;
+//    count++;
+//}
+//switch (count)
+//{
+//    case 2:
+//        Console.WriteLine("2 reqemli");
+//        break;
+//    case 3:
+//        Console.WriteLine("3 reqemli");
+//        break;
+//    case 4:
+//        Console.WriteLine("4 reqemli");
+//        break;
+//    default:
+//        Console.WriteLine("nezere alinmayib");
+//        break;
+//}
+#endregion
+#region 4
+//int num = 8;
+//int count = 0;
+//   if (num % 2 == 0 && num >= 2)
+//   {
+//    count++;
+//        Console.WriteLine("2nin quvvetidir");
+//   }
+//    else
+//    {
+//        Console.WriteLine("2nin quvveti deyil");
+//    }
+#endregion
